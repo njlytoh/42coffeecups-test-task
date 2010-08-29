a7fb8ff74bbf44fb831ef0731e448d66cd982016
Andriy Tomchuk <njlytoh@gmail.com>
2010-08-29 19:27:58 +0300

    updated project middleware settings

--- 42cc_test_task/src/project/project/settings.py
+++ 42cc_test_task/src/project/project/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'history.middleware.RequestHistory',
 )
 
 ROOT_URLCONF = 'project.urls'
